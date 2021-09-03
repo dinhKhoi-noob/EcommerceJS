0052e99b72a3004c2a17990a3e8aae281eea4047
dinhKhoi-noob <khoip1305@gmail.com>
2021-09-04 04:17:33 +0700

    feat(server):Create database and user route

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
-
+const userRoute = require("./controllers/routes/user.js");
 app.set('view engine','ejs');
-
 app.get("/",(req,res)=>{
     res.render(__dirname+"/views/pages/index.ejs")
 });
+app.use(express.json());
+app.use('/auth',userRoute);
 app.use(express.static('public'))
 
 app.listen(4000,()=>{
